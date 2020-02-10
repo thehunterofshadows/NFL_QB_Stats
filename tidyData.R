2e9dec391f8ac2d945cc6b617aa583dab790703c
Justin <47578899+thehunterofshadows@users.noreply.github.com>
2020-02-10 12:03:27 -0600

    updated to include all vars

---
+++ tidyData.R
@@ -0,0 +1,9 @@
+#tidy data
+
+#determine what other fields you need in query
+
+#convert age into somethign useful.  Split out into year and days somehow.  Maybe somethign like 33.3
+
+#convert draft_year to date
+
+#made note game_number is game number that year, not total.
