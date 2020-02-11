3e4e02d595135cdc0339e824807f8930c8e5559f
Justin <47578899+thehunterofshadows@users.noreply.github.com>
2020-02-11 17:47:01 -0600

    adding team colors to the data.table

--- plots.R
+++ plots.R
@@ -4,8 +4,8 @@ library(dplyr)
 library(ggplot2)
 
 yards<-qbData %>%
-  select(name, passing_yards) %>%
-  group_by(name) %>%
+  select(name, passing_yards,current_team) %>%
+  group_by(name,current_team) %>%
   summarise(passing_yards=sum(passing_yards), num_games=n()) %>%
   filter(num_games >36)
             
@@ -13,85 +13,15 @@ yards$yards_per_games<-yards$passing_yards/yards$num_games
 
 yards<-yards[order(-yards$yards_per_games),]
 
-yards<-yards%>%
-  select(name, yards_per_games)
+yards$pri_color<-"#121111"
+yards$sec_color<-"#ffffff"
 
-#v1
-g<-ggplot(data = yards[1:10,]) +
-  geom_bar(mapping= aes(x=reorder(name,-yards_per_games), 
-                        y=yards_per_games, fill=yards_per_games), stat="identity")+
-  theme(
-    axis.text.x=element_text(angle=35,hjust=1, size=12),
-    axis.title.x=element_text(size=14, color="#993333", face="bold"),
-    axis.title.y=element_text(size=14, color="#993333", face="bold"),
-    plot.title=element_text(size=14, color="red", face="bold")
-    ) +
-  labs(title="Yard per Game",x="QB Names",y="Yards per Game")
+yards$pri_color[yards$current_team=="Green Bay Packers"]="#203731"
+yards$sec_color[yards$current_team=="Green Bay Packers"]="#FFB612"
 
-#v2
-g<-ggplot(data = yards[1:10,]) +
-  geom_bar(mapping= aes(x=reorder(name,-yards_per_games), 
-                        y=yards_per_games, fill=yards_per_games), stat="identity")+
-  theme(
-    axis.text.x=element_text(angle=35,hjust=1,vjust=1, size=12),
-    axis.title.x=element_text(size=14, color="#993333", face="bold"),
-    axis.title.y=element_text(size=14, color="#993333", face="bold"),
-    plot.title=element_text(size=14, color="red", face="bold")
-  ) +
-  labs(title="Yard per Game",x="QB Names",y="Yards per Game")+
-  geom_text(mapping=aes(label=yards_per_games,y=yards_per_games/2))
+yards$pri_color[yards$current_team=="Indianapolis Colts"]="#002C5F"
+yards$sec_color[yards$current_team=="Indianapolis Colts"]="#A2AAAD"
 
-#v3
-g<-ggplot(data = yards[1:10,],
-          mapping= aes(x=reorder(name,-yards_per_games), 
-                                           y=yards_per_games, 
-                                           fill=yards_per_games)) +
-  geom_bar( stat="identity")+
-  theme(
-    axis.text.x=element_text(angle=35,hjust=1,vjust=1, size=12),
-    axis.title.x=element_text(size=14, color="#993333", face="bold"),
-    axis.title.y=element_text(size=14, color="#993333", face="bold"),
-    plot.title=element_text(size=14, color="red", face="bold")
-  ) +
-  labs(title="Yard per Game",x="QB Names",y="Yards per Game")+
-  geom_text(mapping=aes(label=name,y=yards_per_games*.75),angle=90,size=8,colour="White") +
-  scale_colour_manual(values="white")
-
-#v4
-#Create way to slide names down the list.  Maybe try getting len of qb name, and use that
-#qbPos<-c(0,5,10,15,20,25,30,35,40,45)
-
-#needed to reset these values as i used a ylim to start the plot at 150
-
-#note use of the reorder to ensure bars are in the right order
-#test
-qbPos<-c(0,2,4,6,8,10,12,14,16,18)
-g<-ggplot(
-  data = yards[1:10,],
-          mapping = aes(x=reorder(name,-yards_per_games), 
-                       y=yards_per_games, 
-                       fill=yards_per_games)
-  )+
-  geom_bar(stat="identity")+
-  theme(
-    axis.text.x=element_text(angle=35,hjust=1,vjust=1, size=12),
-    axis.title.x=element_text(size=14, color="#993333", face="bold"),
-    axis.title.y=element_text(size=14, color="#993333", face="bold"),
-    plot.title=element_text(size=14, color="red", face="bold",hjust=.5)
-  )+
-  labs(
-    title="Yards per Game",x="QB Names",y="Yards per Game"
-    )+
-  geom_text(
-    mapping=aes(label=name,y=((yards_per_games*.9)-qbPos),vjust=.5),
-    angle=90,size=8,colour="White"
-    )+
-  scale_colour_manual(
-    values="white"
-    )+
-  coord_cartesian(
-    ylim=c(150,280)
-  )
 
 
 #v5
@@ -106,7 +36,7 @@ g<-ggplot(
 )+
   geom_bar(
     stat="identity",
-    fill=fillColor
+    fill=yards$pri_color[1:10]
     )+
   theme(
     axis.title.x=element_text(size=14, color="#993333", face="bold"),
@@ -120,7 +50,7 @@ g<-ggplot(
   )+
   geom_text(
     mapping=aes(label=name,y=((yards_per_games*.9)-qbPos),vjust=.5),
-    angle=90,size=8,colour=textColor
+    angle=90,size=8,colour=yards$sec_color[1:10]
   )+
   scale_colour_manual(
     values="white"
