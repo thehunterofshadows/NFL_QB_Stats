435bf6543a564cb1a4a8f944f5c771d8bfb30809
Justin <47578899+thehunterofshadows@users.noreply.github.com>
2020-02-11 08:27:39 -0600

    trying different looks for bar charts.

--- plots.R
+++ plots.R
@@ -1,17 +1,56 @@
 #plots
 library(data.table)
 library(dplyr)
+library(ggplot2)
 
-qbData<-data.table(qbData)
-
-yards<-select(qbData, name, passing_yards)
-yards<-summarise(group_by(yards,name), passing_yards=sum(passing_yards))
-yards<-yards[order(-yards$passing_yards),]
-
-#fix this below using what's above
 yards<-qbData %>%
   select(name, passing_yards) %>%
   group_by(name) %>%
-  summarise(total_passing_yards=sum(passing_yards)) %>%
-  order(total_passing_yards, decreasing = TRUE)
+  summarise(passing_yards=sum(passing_yards), num_games=n()) %>%
+  filter(num_games >36)
+            
+yards$yards_per_games<-yards$passing_yards/yards$num_games
 
+yards<-yards[order(-yards$yards_per_games),]
+
+yards<-yards%>%
+  select(name, yards_per_games)
+
+
+g<-ggplot(data = yards[1:10,]) +
+  geom_bar(mapping= aes(x=reorder(name,-yards_per_games), 
+                        y=yards_per_games, fill=yards_per_games), stat="identity")+
+  theme(
+    axis.text.x=element_text(angle=35,hjust=1, size=12),
+    axis.title.x=element_text(size=14, color="#993333", face="bold"),
+    axis.title.y=element_text(size=14, color="#993333", face="bold"),
+    plot.title=element_text(size=14, color="red", face="bold")
+    ) +
+  labs(title="Yard per Game",x="QB Names",y="Yards per Game")
+
+
+g<-ggplot(data = yards[1:10,]) +
+  geom_bar(mapping= aes(x=reorder(name,-yards_per_games), 
+                        y=yards_per_games, fill=yards_per_games), stat="identity")+
+  theme(
+    axis.text.x=element_text(angle=35,hjust=1,vjust=1, size=12),
+    axis.title.x=element_text(size=14, color="#993333", face="bold"),
+    axis.title.y=element_text(size=14, color="#993333", face="bold"),
+    plot.title=element_text(size=14, color="red", face="bold")
+  ) +
+  labs(title="Yard per Game",x="QB Names",y="Yards per Game")+
+  geom_text(mapping=aes(label=yards_per_games,y=yards_per_games/2))
+
+g<-ggplot(data = yards[1:10,],
+          mapping= aes(x=reorder(name,-yards_per_games), 
+                                           y=yards_per_games, 
+                                           fill=yards_per_games)) +
+  geom_bar( stat="identity")+
+  theme(
+    axis.text.x=element_text(angle=35,hjust=1,vjust=1, size=12),
+    axis.title.x=element_text(size=14, color="#993333", face="bold"),
+    axis.title.y=element_text(size=14, color="#993333", face="bold"),
+    plot.title=element_text(size=14, color="red", face="bold")
+  ) +
+  labs(title="Yard per Game",x="QB Names",y="Yards per Game")+
+  geom_text(mapping=aes(label=name,y=yards_per_games/2),angle=90)
