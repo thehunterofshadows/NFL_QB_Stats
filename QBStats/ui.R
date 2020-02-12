dbbe37bd97b122729d678a689b411875e2cd89ba
Justin <47578899+thehunterofshadows@users.noreply.github.com>
2020-02-12 11:51:40 -0600

    starting shiny app

---
+++ QBStats/ui.R
@@ -0,0 +1,45 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+
+    # Application title
+    titlePanel("QB Stats"),
+
+    # Sidebar with a slider input for number of bins
+    sidebarLayout(
+        sidebarPanel(
+            sliderInput("bins",
+                        "Number of bins:",
+                        min = 1,
+                        max = 50,
+                        value = 30),
+            sliderInput("test",
+                        "testing thisNumber of bins:",
+                        min = 1,
+                        max = 50,
+                        value = 30),
+            selectInput("chartType",
+                        "Chart Type:",
+                        c("Touchdowns to INTs" = "TDtoINT",
+                          "Total Touchdowns" = "TotalTD")
+                
+            )
+            
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+            plotOutput("distPlot")
+        )
+    )
+))
