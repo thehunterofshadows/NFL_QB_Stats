fcb25aad66ab8cd8cd7762ac28eeb3b210d1d863
Justin <47578899+thehunterofshadows@users.noreply.github.com>
2020-02-14 16:51:08 -0600

    cleaned up, but this file isn't needed anymore, i should remove it

--- plots.R
+++ plots.R
@@ -4,62 +4,6 @@ library(dplyr)
 library(ggplot2)
 
 
-tidyData<-function(yards){
-  #setup colors
-  yards$pri_color<-"#121111"
-  yards$sec_color<-"#ffffff"
-  
-  yards$current_team[yards$name=="Peyton Manning"]="Indianapolis Colts"
-  yards$current_team[yards$name=="Kirk Cousins"]="Minnesota Vikings"
-  yards$current_team[yards$name=="Warren Moon"]="Minnesota Vikings"
-  yards$current_team[yards$name=="Fran Tarkenton"]="Minnesota Vikings"
-  yards$name[yards$name=="Brett Favre "]="Brett Favre"
-  yards$current_team[yards$name=="Brett Favre"]="Green Bay Packers"
-  yards$current_team[yards$name=="Dan Marino"]="Miami Dolphins"
-  yards$current_team[yards$name=="John Elway"]="Denver Broncos"
-  
-  yards$pri_color[yards$current_team=="Green Bay Packers"]="#203731"
-  yards$sec_color[yards$current_team=="Green Bay Packers"]="#FFB612"
-  
-  yards$pri_color[yards$current_team=="Indianapolis Colts"]="#002C5F"
-  yards$sec_color[yards$current_team=="Indianapolis Colts"]="#A2AAAD"
-  
-  yards$pri_color[yards$current_team=="Minnesota Vikings"]="#4F2683"
-  yards$sec_color[yards$current_team=="Minnesota Vikings"]="#FFC62F"
-  
-  yards$pri_color[yards$current_team=="Detroit Lions"]="#0076B6"
-  yards$sec_color[yards$current_team=="Detroit Lions"]="#B0B7BC"
-  
-  yards$pri_color[yards$current_team=="Atlanta Falcons"]="#A71930"
-  yards$sec_color[yards$current_team=="Atlanta Falcons"]="#000000"
-  
-  yards$pri_color[yards$current_team=="New Orleans Saints"]="#D3BC8D"
-  yards$sec_color[yards$current_team=="New Orleans Saints"]="#101820"
-  
-  yards$pri_color[yards$current_team=="New England Patriots"]="#002244"
-  yards$sec_color[yards$current_team=="New England Patriots"]="#C60C30"
-  
-  yards$pri_color[yards$current_team=="Los Angeles Rams"]="#002244"
-  yards$sec_color[yards$current_team=="Los Angeles Rams"]="#866D4B"
-  
-  yards$pri_color[yards$current_team=="Los Angeles Chargers"]="#002A5E"
-  yards$sec_color[yards$current_team=="Los Angeles Chargers"]="#FFC20E"
-  
-  yards$pri_color[yards$current_team=="Miami Dolphins"]="#008E97"
-  yards$sec_color[yards$current_team=="Miami Dolphins"]="#FC4C02"
-  
-  yards$pri_color[yards$current_team=="Denver Broncos"]="#FB4F14"
-  yards$sec_color[yards$current_team=="Denver Broncos"]="#002244"
-  
-  yards$pri_color[yards$current_team=="Pittsburgh Steelers"]="#FFB612"
-  yards$sec_color[yards$current_team=="Pittsburgh Steelers"]="#101820"
-  
-  yards$pri_color[yards$current_team=="New York Giants"]="#0B2265"
-  yards$sec_color[yards$current_team=="New York Giants"]="#A71930"
-  
-  yards
-}
-
 pullYardsPerGame<-function(qbData){
 #Pull yards per game data
   yards<-qbData %>%
