b6a1d14d37a98e4998ced2765d53a54ab504f852
Justin <47578899+thehunterofshadows@users.noreply.github.com>
2020-02-17 11:18:07 -0600

    top5 works for passer raiting.  now going to work on makign it dynamic

--- tidyData.R
+++ tidyData.R
@@ -1,6 +1,7 @@
 #tidy data
 library(lubridate)
 library(dplyr)
+library(data.table)
 
 
 tidyData<-function(yards){
@@ -9,7 +10,7 @@ tidyData<-function(yards){
   yards$fixed_passing_attempts<-yards$passing_completions
   
   #pull in the team colors
-  teamColors<-read.csv("./data/teamColors.csv", colClasses = "character")
+  teamColors<-read.csv("teamColors.csv", colClasses = "character")
   
   #create list of which team each player played the most games with.  Then order it so on top is the most played
   mainTeam<-qbData %>%
@@ -44,6 +45,11 @@ tidyData<-function(yards){
 }
 
 topFive<-function(x){
+  #constants
+  min_games = 36
+  top_years = 5
+  min_top_games = 8
+  
   #still trying to figure out how i'm going to do this
   #through it to iterate through each name and for each state pull up the data
   #then filter for top 5, then re-average or retotal
@@ -56,17 +62,11 @@ topFive<-function(x){
     select(name) %>%
     group_by(name) %>%
     summarise(games=n()) %>%
-    filter(games>36)
+    filter(games>min_games)
   names<-namesList$name
   
   for (myName in names){
     #passer rating
-    #need to ensure avg is correct, as in if less then 16 games were played, can't avg seaon
-    #need to split back out to all the games, and avg that.
-    # result <- data.table(
-    #   name = character(),
-    #   passer_rating = numeric()
-    # )
     #  Need to set it up to limit to only QB's with 5 seasons
     yards<- x %>%
       filter(myName==name) %>%
@@ -74,22 +74,24 @@ topFive<-function(x){
       select(year, passing_rating, pri_color, sec_color) %>%
       group_by(year, pri_color, sec_color) %>%
       summarise(y=mean(passing_rating), games = n()) %>%
-        filter(games>8)
+        filter(games>min_top_games)
       
-    
-    yards<-yards[order(-yards$y),]
-    myYears<-yards$year[1:5]
-    
-    yards<- x %>%
-      filter(myName==name, year %in% myYears) %>%
-      select(passing_rating, pri_color, sec_color) %>%
-      group_by(pri_color, sec_color) %>%
-      summarise(y=mean(passing_rating))
-    
-    passer_rating<-yards$y
-    myName<-as.character(myName)
-    results<-rbind(results, data.table(name=myName, passer_rating=passer_rating, years=paste(myYears,sep = " ")))
-    
-  }
+    if (nrow(yards)>=top_years){
+        yards<-yards[order(-yards$y),]
+        myYears<-yards$year[1:top_years]
+        
+        yards<- x %>%
+          filter(myName==name, year %in% myYears) %>%
+          select(passing_rating, pri_color, sec_color) %>%
+          group_by(pri_color, sec_color) %>%
+          summarise(y=mean(passing_rating))
+        
+        passer_rating<-yards$y
+        myName<-as.character(myName)
+        myYear<-paste(myYears,collapse = " ")
+        results<-rbind(results, data.table(name=myName, passer_rating=passer_rating, years=myYear))
+      }
+    }
+  results<-results[order(results$passer_rating,decreasing = TRUE),]
   results
-}
+  }
