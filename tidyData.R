950fbad102b9c2b9a9859b8b409fbc3022158023
Justin <47578899+thehunterofshadows@users.noreply.github.com>
2020-02-17 16:56:55 -0600

    Updated topRating to be fully dynamic.  Now it must be put into the shiny app

--- tidyData.R
+++ tidyData.R
@@ -8,18 +8,18 @@ tidyData<-function(yards){
   
   #limit to QB's with at least 5 years of 8+ games
   myNames <- unique(yards %>%
-    select(name, year,date) %>%
-    group_by(name, year) %>%
-    summarise(num_games=n()) %>%
-    filter(num_games >= 8) %>%
-      select(name, year) %>%
-      group_by(name) %>%
-      summarise(years=n()) %>%
-      filter(years>=5) %>%
-    select(name))
+                      select(name, year,date) %>%
+                      group_by(name, year) %>%
+                      summarise(num_games=n()) %>%
+                      filter(num_games >= 8) %>%
+                      select(name, year) %>%
+                      group_by(name) %>%
+                      summarise(years=n()) %>%
+                      filter(years>=5) %>%
+                      select(name))
   
   yards<-yards[yards$name %in% myNames$name,]
-    
+  
   
   #the data has the completions and attempts flipped.
   yards$fixed_passing_completions<-yards$passing_attempts
@@ -54,7 +54,7 @@ tidyData<-function(yards){
     yards$pri_color[i]<-teamColors$priColor[teamColors$team==team]
     yards$sec_color[i]<-teamColors$sndColor[teamColors$team==team]
   }
-    
+  
   
   #return the data to the function
   yards
@@ -90,24 +90,24 @@ topFive<-function(x){
       select(year, passing_rating, pri_color, sec_color) %>%
       group_by(year, pri_color, sec_color) %>%
       summarise(y=mean(passing_rating), games = n()) %>%
-        filter(games>min_top_games)
-      
+      filter(games>min_top_games)
+    
     if (nrow(yards)>=top_years){
-        yards<-yards[order(-yards$y),]
-        myYears<-yards$year[1:top_years]
-        
-        yards<- x %>%
-          filter(myName==name, year %in% myYears) %>%
-          select(passing_rating, pri_color, sec_color) %>%
-          group_by(pri_color, sec_color) %>%
-          summarise(y=mean(passing_rating))
-        
-        passer_rating<-yards$y
-        myName<-as.character(myName)
-        myYear<-paste(myYears,collapse = " ")
-        results<-rbind(results, data.table(name=myName, passer_rating=passer_rating, years=myYear))
-      }
+      yards<-yards[order(-yards$y),]
+      myYears<-yards$year[1:top_years]
+      
+      yards<- x %>%
+        filter(myName==name, year %in% myYears) %>%
+        select(passing_rating, pri_color, sec_color) %>%
+        group_by(pri_color, sec_color) %>%
+        summarise(y=mean(passing_rating))
+      
+      passer_rating<-yards$y
+      myName<-as.character(myName)
+      myYear<-paste(myYears,collapse = " ")
+      results<-rbind(results, data.table(name=myName, passer_rating=passer_rating, years=myYear))
     }
+  }
   results<-results[order(results$passer_rating,decreasing = TRUE),]
   results
 }
@@ -117,19 +117,25 @@ topRating<-function(x){
   #constants
   min_games = 36
   top_years = 5
+  top_yearsTF = TRUE
   min_top_games = 8
-  # stat_value = "passing_rating"
-  stat_value = "passing_yards"
+  
+  stat_value = "passing_rating"
+  #stat_value = "passing_yards"
+  #stat_value = "passing_touchdowns"
   stat_select = list(stat_value,"year","pri_color","sec_color")
   stat_group = list("year","pri_color","sec_color")
-  summ <- paste0('mean(', stat_value, ')')  # construct summary method, e.g. mean(mpg)
-  summ_name <- paste0('mean_', stat_value)
+  #summ <- paste0('mean(', stat_value, ')')  # construct summary method, e.g. mean(mpg)
+  #summ_name <- paste0('mean_', stat_value)
+  
+  summ <- paste0('sum(', stat_value, ')')  # construct summary method, e.g. mean(mpg)
+  summ_name <- paste0('sum_', stat_value)
   
   
   #after years are figured out
   noYear_stat_select = list(stat_value,"pri_color","sec_color")
   noYear_stat_group = list("pri_color","sec_color")
-
+  
   
   #still trying to figure out how i'm going to do this
   #through it to iterate through each name and for each state pull up the data
@@ -149,29 +155,43 @@ topRating<-function(x){
   names<-namesList$name
   
   for (myName in names){
-    #passer rating
-    #  Need to set it up to limit to only QB's with 5 seasons
-    yards<- x %>%
-      filter(myName==name) %>%
-      filter(passing_attempts>11) %>%
-      select_(.dots = stat_select) %>%
-      group_by_(.dots = stat_group) %>%
-      summarise_(y=(.dots = setNames(summ, summ_name)), games = (.dots="n()")) %>%
-        filter(games>min_top_games)
+    #reset number of good years
+    number_of_years<-top_years
     
-    if (nrow(yards)>=top_years){
-      yards<-yards[order(-yards$y),]
-      myYears<-yards$year[1:top_years]
-      
+    if (top_yearsTF){
       yards<- x %>%
-        filter(myName==name, year %in% myYears) %>%
+        filter(myName==name) %>%
+        filter(passing_attempts>11) %>%
+        select_(.dots = stat_select) %>%
+        group_by_(.dots = stat_group) %>%
+        summarise_(y=(.dots = setNames(summ, summ_name)), games = (.dots="n()")) %>%
+        filter(games>min_top_games)
+      number_of_years<-nrow(yards)
+      
+      if (number_of_years>=top_years){
+        yards<-yards[order(-yards$y),]
+        myYears<-yards$year[1:top_years]
+        
+        yards<- x %>%
+          filter(myName==name, year %in% myYears) %>%
+          select_(.dots=noYear_stat_select) %>%
+          group_by_(.dots=noYear_stat_group) %>%
+          summarise_(y=(.dots = setNames(summ, summ_name)))
+      }
+    } else if (!top_yearsTF) {
+      yards<- x %>%
+        filter(myName==name) %>%
         select_(.dots=noYear_stat_select) %>%
         group_by_(.dots=noYear_stat_group) %>%
         summarise_(y=(.dots = setNames(summ, summ_name)))
-      
+      myYears<-"All"
+    }
+    if (number_of_years>=top_years){
       passer_rating<-yards$y
       myName<-as.character(myName)
       myYear<-paste(myYears,collapse = " ")
+      number_of_years<-nrow(yards)
+      
       results<-rbind(results, data.table(name=myName, passer_rating=passer_rating, years=myYear))
     }
   }
